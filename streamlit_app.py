8bd78c82db848624436dff7749b4443c0e76845b
Arvind Iyengar <arvi.iyengar@gmail.com>
2025-03-12 12:32:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Summariser")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses OpenAI's models to generate summaries for any website. "
+    "To use this feature, please provide any website link that you wanna summarise"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
